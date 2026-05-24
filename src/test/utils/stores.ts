16da36533a9188d55555bcbe60ec2228286c0bfe
Ademir José Ferreira Júnior <ademirfel.ferreira@gmail.com>
2026-05-24 13:02:54 -0300

    feat: expand persisted MVP settings

--- src/test/utils/stores.ts
+++ src/test/utils/stores.ts
@@ -1,9 +1,32 @@
 import { useSessionStore, type SessionState } from "@/stores/session";
-import { useSettingsStore, type SettingsState } from "@/stores/settings";
+import {
+  getSystemDefaultLineEnding,
+  useSettingsStore,
+  type SettingsState,
+} from "@/stores/settings";
 
 export function setDefaultSettings(settings: Partial<SettingsState> = {}) {
   useSettingsStore.setState({
     theme: "system",
+    recordRecentItems: true,
+    recentFiles: [],
+    recentFolders: [],
+    sidebarVisible: true,
+    fileTreeSortOrder: "name",
+    defaultNewDocumentExtension: ".md",
+    defaultNewDocumentLineEnding: getSystemDefaultLineEnding(),
+    insertFinalNewline: true,
+    indexFileNames: ["readme", "index"],
+    ignoredDirectories: [
+      ".git",
+      ".hg",
+      ".svn",
+      "node_modules",
+      "target",
+      "dist",
+      "build",
+      ".cache",
+    ],
     autoPairBracketsAndQuotes: true,
     softWrapCodeBlocks: false,
     ...settings,

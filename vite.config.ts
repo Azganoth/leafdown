938b9bb8a513ff52b6db23a4f5ff79bad7d28851
Ademir José Ferreira Júnior <ademirfel.ferreira@gmail.com>
2026-06-12 04:31:22 -0300

    refactor: use native tsconfig path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 const host = process.env.TAURI_DEV_HOST;
 
 export default defineConfig({
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    tsconfigPaths: true,
   },
   plugins: [react(), tailwindcss()],
   clearScreen: false,

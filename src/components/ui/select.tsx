b3c5f9146e767b49eedc0d327ef87d01bf12d976
Ademir José Ferreira Júnior <ademirfel.ferreira@gmail.com>
2026-09-23 15:58:12 -0300

    Consolidate shared UI utilities and navigator notices

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,7 +1,8 @@
 import { Select as SelectPrimitive } from "@base-ui/react/select";
-import { cn } from "cn";
 import { ChevronDownIcon, CheckIcon, ChevronUpIcon } from "lucide-react";
 import * as React from "react";
+
+import { cn } from "@/lib/utils";
 
 const Select = SelectPrimitive.Root;
 
